8e66f647dbda40ce41645d80393019ad41676796
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2022-03-13 17:26:26 +0300

    fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,10 @@
-require('./jvgsc')
 require("jquery")
+import jquery from "jquery"
+window.jQuery = jquery
+window.$ = jquery
+
+require('./jvgsc')
+
 // require("./jsspeccy")
 import "@fortawesome/fontawesome-free/js/all"
 // import 'popper.js'
